3e8b491a1e858cce1426605114b6e47a70ea0c1d
BTram <bichtramphamngo@gmail.com>
2025-11-10 12:03:04 +0700

    feat(service): update interface reservation services

--- SmartRestaurantSystem/BusinessLogicLayer/Services/Interfaces/IReservationService.cs
+++ SmartRestaurantSystem/BusinessLogicLayer/Services/Interfaces/IReservationService.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessObjects.Models;
+
 
 namespace BusinessLogicLayer.Services.Interfaces
 {
-    internal class IReservationService
+    public interface IReservationService
     {
+        List<Reservation> GetAll();
+        Reservation GetById(int id);
+        List<Reservation> GetUpcoming();
+        void Add(Reservation r);
+        void Update(Reservation r);
+        void Delete(int id);
+
+        void AssignTable(int reservationId, int tableId);
     }
 }
